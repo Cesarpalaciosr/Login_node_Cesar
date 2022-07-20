639d48cdcccff884852a658312858bd01211f3fd
CesarP <{ID}+{username}@users.noreply.github.com>
2022-07-20 17:18:14 -0400

    register

--- src/scripts/register.js
+++ src/scripts/register.js
@@ -1,36 +1,37 @@
 var registerForm = document.getElementById("data-forms");
-var inputUsername = document.getElementById("input-username");
-var inputPassword = document.getElementById("input-password");
+var API_url = "http://localhost:3000/api/register";
+
 
 registerForm.addEventListener("submit", send);
 
 async function send(e) {
   e.preventDefault();
+  var data = {};
+  var form = new FormData(registerForm);
+  for (var pair of form.entries()) {
+    data[pair[0]] = pair[1];
+  }
+  
+  console.log(data);
+  json = JSON.stringify(data);
 
-  const data = {
-    username: inputUsername.value,
-    password: inputPassword.value,
-  };
-
-  var res = await fetch("/api/register", {
+  console.log(json)
+  var res = await fetch(API_url, {
     headers: {
-      Accept: "application/json",
-      "Content-Type": "application/json",
+      'Accept': 'application/json',
+      'Content-Type': 'application/json'
     },
     method: "POST", // or 'PUT'
+    
+    body:json,
+  }).catch((error) => console.error("Error:", error));
+  
+  var response = await res;
 
-    body: JSON.stringify(data),
-  }).catch((error) => {
-    console.error("Error:", error);
-    alert(error.detail);
-  });
-
-  const response = await res.json();
-
-  if (response.status === 200) {
+  if (response.status == 200) {
+	console.log(window.location)
     window.location.href = "./views/login.html";
   } else {
-    alert(response.detail);
     console.log("No se pudo Registrar los datos");
   }
 }
